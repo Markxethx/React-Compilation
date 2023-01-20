7ea5ff39e783f2e2bf9068bcac058e018a2f4d99
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-01-20 22:29:47 +0800

    Update tailwind.config.cjs

--- my-protfolio/tailwind.config.cjs
+++ my-protfolio/tailwind.config.cjs
@@ -5,7 +5,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary : "#00040F",
+      },
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      smd: "840px",
+      md: "1060px",
+      lg: "1280px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
