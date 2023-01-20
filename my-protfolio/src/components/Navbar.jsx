1bcf58a6909744e063dbfa3e0d61b3ff394cbb74
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-01-20 22:29:24 +0800

    Update Navbar.jsx

--- my-protfolio/src/components/Navbar.jsx
+++ my-protfolio/src/components/Navbar.jsx
@@ -44,32 +44,25 @@ const Navbar = () => {
               onClick={() => setToggle(!toggle)} />
         </div>
 
-        <div className={`${!toggle ? "hidden" : "absolute"} z-10 flex h-screen w-screen bg-blue-900`}>
-            <div className='flex h-full w-full'>
+        <div className={`${!toggle ? "hidden" : "fixed"} smd:hidden z-10 top-[90px] flex h-screen w-full bg-primary  `}>
 
-              <div className='flex flex-1 justify-end items-center m-[20px]'>
-                <img
-                  src={`${!toggle ? menu : close}`}
-                  alt="menu"
-                  className='w-[28px] h-[28px] object-contain'
-                  onClick={() => setToggle(!toggle)} />
+              <div className='flex w-full justify-center side-bar items-center'>
+                <ul className={`flex-col mt-[-100px] text-center ease-in duration-300`}>
+                  {navLinks.map((nav, index) => (
+                    <li
+                      key={nav.id}
+                      className={`font-poppins font-normal mb-10 cursor-pointer text-[20px] ${
+                        active === nav.title ? "text-gradient" : "text-natural"
+                      }`}
+                      onClick={() => setActive(nav.title)}
+                    >
+
+                      <a href={`#${nav.id}`}>{nav.title}</a>
+                    </li>
+                  ))}
+                </ul>
               </div>
 
-              <ul className={`flex-col absolute top-20`}>
-                {navLinks.map((nav, index) => (
-                  <li
-                    key={nav.id}
-                    className={`font-poppins font-normal cursor-pointer text-[16px] ${
-                      active === nav.title ? "text-red-900" : "text-blue-900"
-                    } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-                    onClick={() => setActive(nav.title)}
-                  >
-
-                    <a href={`#${nav.id}`}>{nav.title}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
         </div>
     </nav>
   )
