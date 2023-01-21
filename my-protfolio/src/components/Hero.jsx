e28169d58f848d34dc4f84bff5171601b1d656ff
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-01-22 02:17:44 +0800

    Update Hero.jsx

--- my-protfolio/src/components/Hero.jsx
+++ my-protfolio/src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from '../style';
-import { vectors, Me } from "../assets";
+import { vectors, Justme } from "../assets";
 
 const Hero = () => (
-  <section className={`text-white w-full flex-col md:flex-row ${styles.flexBetween} ${styles.paddingX}`}>
+  <section className={`text-white w-full h-auto flex-col md:flex-row px-10 relative pb-[500px] ${styles.flexBetween} ${styles.paddingX}`}>
 
     <div>
       <div className='relative'>
@@ -11,21 +11,32 @@ const Hero = () => (
           <img src={vectors} alt="vector" className='w-[818px] h-[438px]' />
         </div>
 
-        <div className='lg:w-[818px] md:w-[418px] w-full h-auto smd:h-[438px]'>
-          <h2 className={`${styles.title} lg:text-[64px] md:text-[40px]`}>Passionate</h2>
-          <h1 className={`${styles.title} lg:text-[64px] md:text-[40px] text-brown`}>Front End Developer</h1>
-          <h2 className={`${styles.title} lg:text-[64px] md:text-[40px]`}>From Philippines</h2>
-          <h3 className='font-poppins non-italic font-bold  text-[18px]'>Hi i am Mark John Factor Saspa</h3>
-          <p className='font-poppins non-italic font-bold  text-[18px] dim-light'>A freelance problem solver and critical thinker. Knowledge of </p>
-          <p className='font-poppins non-italic font-bold  text-[18px] dim-light'>Engineering and Mathematics. With our world of continuous growth </p>
-          <p className='font-poppins non-italic font-bold  text-[18px] dim-light'>and understanding i therefore love exploring new things and </p>
-          <p className='font-poppins non-italic font-bold  text-[18px] dim-light'>technologies.</p>
+        <div className='w-full h-auto'>
+          <h2 className={`${styles.title} ${styles.paddingText}`}>Passionate</h2>
+          <h1 className={`${styles.title} ${styles.paddingText} text-brown`}>Front End Developer</h1>
+          <h2 className={`${styles.title} ${styles.paddingText}`}>From Philippines</h2>
+          <h3 className={`font-poppins non-italic font-bold  text-[18px] ${styles.paddingText}`}>Hi i am Mark John Factor Saspa</h3>
+          <p className={`${styles.text}`}>A freelance problem solver and critical thinker. Knowledge of </p>
+          <p className={`${styles.text}`}>Engineering and Mathematics. With our world of continuous growth </p>
+          <p className={`${styles.text}`}>and understanding i therefore love exploring new things and </p>
+          <p className={`${styles.text}`}>technologies.</p>
         </div>
       </div>
     </div>
 
     <div className='smd:w-[694px] sm:w-[500px] w-[350px]'>
-      <img src={Me} alt="Myself" />
+      <div className='relative'>
+        <img src={Justme} className="z-20" alt="Myself" />
+        <div className="absolute top-[-50px] left-[-50px] wheel z-0">
+          <div className="cabin z-10"></div>
+          <div className="cabin z-10"></div>
+          <div className="cabin z-10"></div>
+          <div className="cabin z-10"></div>
+          <div className="cabin z-10"></div>
+          <div className="cabin z-10"></div>
+        </div>
+
+    </div>
     </div>
 
   </section>
