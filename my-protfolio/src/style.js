a86410028de8a11b327215df462a985398ed0fbb
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-01-22 02:18:04 +0800

    Update style.js

--- my-protfolio/src/style.js
+++ my-protfolio/src/style.js
@@ -1,14 +1,16 @@
 const styles = {
     title : "font-poppins leading-[70px] font-semibold non-italic text-[64px]",
     paragraph : "font-poppins non-italic text-lg tracking-wider",
+    text: "font-poppins non-italic font-bold  text-[18px] dim-light",
+    paddingText: "lg:mb-[20px] md:mb-[10px] mb-3",
 
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start",
+    flexBetween : "flex justify-between items-center",
 
     navbar : "font-poppins non-italic font-semibold leading-6 text-[18px]",
-    paddingX: "sm:px-16 px-6",
+    paddingX: "sm:px-14",
     paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-12 py-4",
   
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
