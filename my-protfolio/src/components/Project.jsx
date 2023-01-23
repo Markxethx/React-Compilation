981e94af5621712a6c7ded1bbf54e5892611b903
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-01-23 22:39:10 +0800

    Update Project.jsx

--- my-protfolio/src/components/Project.jsx
+++ my-protfolio/src/components/Project.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from "../style";
+import { expertise } from '../constants';
+import { projects, business } from "../assets";
 
 const Project = () => {
   return (
-    <div>
-        <div className={`${styles.flexBetween} flex-col smd:flex-row px-10 mt-10 md:mt-0 md:mb-10 mb-5`}>
-            <img src={expertiseLogo} className="ml-0 hidden smd:flex lg:ml-28 w-[418px] h-[418px]" alt="Image Logo" />
+        <div id='projects' className={`${styles.flexBetween} flex-col smd:flex-row-reverse px-10 mt-10 md:mt-0 md:mb-10 mb-5`}>
+            <img src={business} className="mr-0 hidden smd:flex lg:mr-28 w-[418px] h-[418px]" alt="Image Logo" />
             
-            <div className='w-full md:w-[660px] text-natural text-end'>
+            <div className='w-full md:w-[660px] text-natural text-start'>
 
                 <div className='flex flex-row items-center'>
-                    <img src={tools} className='w-[120px] h-[120px]' alt="tools logo" />
-                    <h2 className={`ml-5 mb-5 font-poppins leading-[70px] font-semibold non-italic text-[64px] text-gradient`}>Skills and Tools</h2>
+                    <h2 className={`mr-5 mb-5 font-poppins leading-[70px] font-semibold non-italic text-[64px] text-gradient`}>Skills and Tools</h2>
+                    <img src={projects} className='w-[120px] h-[120px]' alt="tools logo" />
                 </div>
-                    <p className={`${styles.text}`}>With the use of modern technologies i love 
-                    to build useful application and solve modern
-                    problems alongside with people i work with.
-                    I use to learn and adopt new things easily 
-                    especially with new environment. And with the 
-                    help of my strong engineering and problem solving
-                    skills i use to learn faster new languages and frameworks.</p>
+                    <p className={`${styles.text}`}>I am at my highest point when
+                     i am able to apply all the skills 
+                     and knowledge that i have to real world 
+                     application. Learning and contributing to 
+                     meaningful projects. </p>
 
             </div>
         </div>
-    </div>
   )
 }
 
-export default Project
+export default Project;
