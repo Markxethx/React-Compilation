bb72a03e143ff6bf942d6d6165b7d2c1d97e70a1
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-01-29 16:25:59 +0800

    Add files via upload

--- my-protfolio/src/constants/index.js
+++ my-protfolio/src/constants/index.js
@@ -1,23 +1,53 @@
-import {html, css, sass, javascript, bootstrap, tailwind, figma, shopify, react ,php } from "../assets";
+import {html,
+    css,
+    sass,
+    javascript,
+    bootstrap,
+    tailwind,
+    figma,
+    shopify,
+    react,
+    php,
+    resume,
+    mode,
+    startup,
+    chatgpt,
+    gericht,
+    potter,
+    weatherapp,
+    pokemon,
+    translator,
+    hoobank,
+    metricImperial,
+    blog,
+    portfolio,
+    journey,
+    developer,
+    up,
+    github,
+    twitter,
+    linkedln,
+    gmail,
+} from "../assets";
 
 export const navLinks = [
     {
-        id: "skills",
-        title: "Skills",
+      id: "skills",
+      title: "Skills",
     },
     {
-        id: "projects",
-        title: "Projects",
+      id: "projects",
+      title: "Projects",
     },
     {
-        id: "education",
-        title: "Education",
+      id: "education",
+      title: "Education",
     },
     {
-        id: "contacts",
-        title: "Contacts",
+      id: "contacts",
+      title: "Contacts",
     },
-];
+  ];
 
 export const home = [
     {
@@ -26,14 +56,14 @@ export const home = [
     }
 ];
 
-export const mode = [
+export const modes = [
     {
-        id : download,
-        icon : moonChanges,
+        id : "download",
+        icon : mode,
     },
     {
-        id : resume,
-        icon : book,
+        id : "resume",
+        icon : resume,
     }
 ];
 
@@ -96,29 +126,176 @@ export const expertise = [
     }
 ];
 
+export const projectItems1 = [
+    {
+        id: "startup",
+        icon: startup,
+        lang1: "JavaScript",
+        lang2: "CSS",
+        text: "My StartUp Journey and as a freelancer ...",
+        web: "https://pickygrinder.vercel.app",
+        github: "https://github.com/Markxethx/Picky-Grinder",
+    },
+    {
+        id: "chatgpt",
+        icon: chatgpt,
+        lang1: "React",
+        lang2: "CSS",
+        text: "GPT-3 that describes the future of OPENAI",
+        web: "https://chatgptisfuture.netlify.app",
+        github: "https://github.com/Markxethx/React-Compilation/tree/main/Website%20%23%201",
+    },
+    {
+        id: "potter",
+        icon: potter,
+        lang1: "JavaScript",
+        lang2: "CSS",
+        text: "Convert a letter to harry potter theme.",
+        web: "https://harrypotterlettertheme.netlify.app",
+        github: "https://github.com/Markxethx/All-About-JavaScript-Mastery/tree/main/Project%2325-Harry%20Potter%20Theme%20letter",
+    },
+    {
+        id: "gericht",
+        icon: gericht,
+        lang1: "React",
+        lang2: "CSS",
+        text: "Quality UI/UX for online e-commerece restaurant",
+        web: "https://restobargerich.netlify.app",
+        github: "https://github.com/Markxethx/React-Compilation/tree/main/Website%232",
+    },
+    {
+        id: "weatherapp",
+        icon: weatherapp,
+        lang1: "JavaScript",
+        lang2: "Tailwind",
+        text: "Fetch API that gives data to realtime weather to a City all over the world",
+        web: "https://weatherweatherph.netlify.app",
+        github: "https://github.com/Markxethx/All-About-JavaScript-Mastery/tree/main/Project%2322-Weather%20App",
+    },
+    {
+        id: "pokemon",
+        icon: pokemon,
+        lang1: "Javascript",
+        lang2: "Tailwind",
+        text: "Cards that shows the power and details of pokemon. Using Fetch Api.",
+        web: "https://pokemoncards101.netlify.app",
+        github: "https://github.com/Markxethx/All-About-JavaScript-Mastery/tree/main/pokemonGo",
+    },
+    {
+        id: "potter",
+        icon: translator,
+        lang1: "JavaScript",
+        lang2: "CSS",
+        text: "Convert a word to any selected language existing in this world.",
+        web: "https://translateurwords.netlify.app",
+        github: "https://github.com/Markxethx/All-About-JavaScript-Mastery/tree/main/Project%2324-Word%20Translator",
+    },
+    {
+        id: "hoobank",
+        icon: hoobank,
+        lang1: "React",
+        lang2: "Tailwind",
+        text: "Quality designs for professional website. The next generations of payment",
+        web: "https://hoobankcoder.netlify.app",
+        github:"https://github.com/Markxethx/All-About-JavaScript-Mastery/tree/main/pokemonGo",
+    },
+    {
+        id: "metricImperial",
+        icon: metricImperial,
+        lang1: "JavaScript",
+        lang2: "CSS",
+        text: "Convert any metric/imperial to its equivalent value.",
+        web: "https://metricconversion.netlify.app",
+        github:"https://github.com/Markxethx/All-About-JavaScript-Mastery/tree/main/Project%2317-ConvertMetric",
+    },
+    {
+        id: "blog",
+        icon: blog,
+        lang1: "Javascript",
+        lang2: "CSS",
+        text: "My personal blog as a developer that explains on why Programming is hard.",
+        web: "https://whyprogramming-is-hard.netlify.app",
+        github:"https://github.com/Markxethx/why-programming-is-hard-",
+    },
+    {
+        id: "potterss",
+        icon: potter,
+        lang1: "JavaScript",
+        lang2: "CSS",
+        text: "Convert a letter to harry potter theme.",
+        web: "",
+        github:"",
+    },
+    {
+        id: "portfolio",
+        icon: portfolio,
+        lang1: "React",
+        lang2: "Tailwind",
+        text: "My portfolio that best describes my design and coding skills",
+        web: "https://myportblank.netlify.app",
+        github:"https://github.com/Markxethx/React-Compilation/tree/main/my-protfolio",
+    },
+];
+
+export const link = [
+    {
+        id: "id1"
+    },
+    {
+        id: "id2"
+    },
+    {
+        id: "id3"
+    },
+    {
+        id: "id4"
+    }
+]
+
 export const title = [
     {
         id: "exp-one",
-        title : "Remote Freelance Developer",
-        content : "August 2022 - present"
+        title : "Remote Freelance",
+        content : "August 2022 - present",
+        icon: up,
     },
     {
         id: "exp-two",
         title : "Html Scss Developer",
-        content : "July 2022 - September"
+        content : "July 2022 - September",
+        icon: developer,
     },
     {
         id: "exp-three",
         title : "Startup Journey",
-        content : "September 2022 - present"
+        content : "September 2022 - present",
+        icon: journey,
     }
 ];
 
-export const contacts = [
+export const info = [
     {
         id: "gmail",
-        icon : "",
-        content : "",
-        links: ""
-    }
+        content : "markjohnsaspa@gmail.com",
+        icon: gmail,
+        links: "https://mail.google.com/mail/u/5/#inbox",
+    },
+    {
+        id: "twitter",
+        content : "twitter.com/the_blank_eth",
+        icon: twitter,
+        links: "https://twitter.com/the_blank_eth"
+    },
+    {
+        id: "linkedln",
+        icon: linkedln,
+        content : "linkedln/Saspa-Mark-John",
+        links: "https://www.linkedin.com/in/saspa-mark-john-f-515a49234/",
+    },
+    {
+        id: "github",
+        content : "https://github.com/Markxethx",
+        icon: github,
+        links: "https://github.com/Markxethx?tab=overview&from=2023-01-01&to=2023-01-27"
+    },
 ]
