2b1d4c018f27616a36d85672d7b850ead34b8cc1
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-02-16 22:14:23 +0800

    Update App.jsx

--- my-protfolio/src/App.jsx
+++ my-protfolio/src/App.jsx
@@ -1,31 +1,40 @@
 import styles from "./style";
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import { Navbar, Hero, Project, Skills, Education, Code } from "./components";
+import { Navbar, Hero, Project, Skills, Education, Code, Loading } from "./components";
 
-const App = () => 
-{
+const App = () => {
   const [changeBackGroundColor, setChangeBackGroundColor] = useState(false);
+  const [loadingIntro, setLoadingIntro] = useState(true)
+  function loading() {
+    setLoadingIntro(false)
+  }
+  
+  useEffect( () => {
+    setTimeout(loading, 3000)
+  }, [])
 
   return (
-  <div className={`${changeBackGroundColor ? "bg-gray-50" : "bg-primary"} w-full overflow-hidden relative`}>
-    <div className={`${styles.paddingX}`}>
-      <div className={`${styles.flexStart}`}>
-        <Navbar onclick={() => setChangeBackGroundColor(!changeBackGroundColor)}
-        darkMode={changeBackGroundColor}/>
-      </div>
+  <>{ loadingIntro ? <Loading/> : <div className={`${changeBackGroundColor ? "bg-gray-50" : "bg-primary"} w-full overflow-hidden relative`}>
+  <div className={`${styles.paddingX}`}>
+    <div className={`${styles.flexStart}`}>
+      <Navbar onclick={() => setChangeBackGroundColor(!changeBackGroundColor)}
+      darkMode={changeBackGroundColor}/>
     </div>
-
-    <div className={`${styles.paddingX}`}>
-      <Hero darkMode={changeBackGroundColor}/>
-      <Skills darkMode={changeBackGroundColor}/>
-      <Project darkMode={changeBackGroundColor}/>
-      <Education darkMode={changeBackGroundColor}/>
-      <Code darkMode={changeBackGroundColor}/>
-    </div>
-
   </div>
+
+  <div className={`${styles.paddingX}`}>
+    <Hero darkMode={changeBackGroundColor}/>
+    <Skills darkMode={changeBackGroundColor}/>
+    <Project darkMode={changeBackGroundColor}/>
+    <Education darkMode={changeBackGroundColor}/>
+    <Code darkMode={changeBackGroundColor}/>
+  </div>
+
+</div>}
+  </>
+  
 )};
 
 export default App
